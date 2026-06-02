c0ae0cd113203500751ac585f5ab2b92edd449bf
Yvonne <yvonne101479@gmail.com>
2026-06-02 14:03:56 +0800

    新增資料庫連線方法

---
+++ DBConnection.py
@@ -0,0 +1,19 @@
+'''
+docker run --name mysql-container -p 3307:3306 -e MYSQL_ROOT_PASSWORD=1234567890 -d mysql:latest
+'''
+
+# 引入必要套件
+import pymysql
+
+# 設定資料庫連線資訊
+host = 'localhost'
+port = 3306  
+user = 'root'
+passwd = 'password'
+db = 'TESTDB'
+charset = 'utf8mb4'
+
+
+def get_conn():
+    conn = pymysql.connect(host=host, port=port, user=user, passwd=passwd, db=db, charset=charset)
+    
